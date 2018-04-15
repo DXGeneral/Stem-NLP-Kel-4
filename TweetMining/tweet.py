a5702c4116e235ff8949801cfe8c67a53af1b4af
Ahmad Idham T Lubis <ofan.lubis@gmail.com>
2018-04-15 14:10:01 +0700

    Udah jalan tapi masih terbatas jumlah tweet-nya

--- TweetMining/tweet.py
+++ TweetMining/tweet.py
@@ -1,15 +1,15 @@
 import tweepy
 from tweepy import OAuthHandler
  
-consumer_key = 'tbXUij0Auakryoljyh6GbRTjT'
-consumer_secret = 'l5BX4AvA5sr1rSTk7m82kyxdWSr6App7TamvCOVhTqg5gcQ1uK'
+consumer_key = 'OU4n6gpftucZHbF727ylx6pe3'
+consumer_secret = '9G2RBxpTCQwXiUqhLlpvrnj4arzOLTKbAPhEhfSYJQs5Uv0onD'
 access_token = '321787319GSH9sxzlA9qsJFp9NnWXu4aBJyjQFy5tIw49LOef'
-access_secret = ' n4bub6B82yAfqTgzYm4vQLCb2a0JPas6aGPMtb2gX08Ly'
+access_secret = 'n4bub6B82yAfqTgzYm4vQLCb2a0JPas6aGPMtb2gX08Ly'
  
 auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+#auth.set_access_token(access_token, access_secret)
  
 api = tweepy.API(auth)
-for tweet in api.search(q="google"):
+for tweet in api.search(q="jaringan", rpp=100):
 	print(tweet.text)
 	print('\n')
