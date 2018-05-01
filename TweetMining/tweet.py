9799d414d3d14a91168d48d82ec3469de260a6f4
Ahmad Idham T Lubis <ofan.lubis@gmail.com>
2018-05-01 16:21:03 +0700

    Fungsi perhitungan bobot

--- TweetMining/tweet.py
+++ TweetMining/tweet.py
@@ -8,7 +8,7 @@ consumer_secret = '9G2RBxpTCQwXiUqhLlpvrnj4arzOLTKbAPhEhfSYJQs5Uv0onD'
 access_token = '321787319GSH9sxzlA9qsJFp9NnWXu4aBJyjQFy5tIw49LOef'
 access_secret = 'n4bub6B82yAfqTgzYm4vQLCb2a0JPas6aGPMtb2gX08Ly'
  
-auth = OAuthHandler(consumer_key, consumer_secret)
+#auth = OAuthHandler(consumer_key, consumer_secret)
 #auth.set_access_token(access_token, access_secret)
 
 """
@@ -35,11 +35,51 @@ factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
 # stem
-sentence = 'Perekonomian Indonesia sedang dalam pertumbuhan yang membanggakan'
-output   = stemmer.stem(sentence)
-
-print(output)
+#sentence = 'Perekonomian Indonesia sedang dalam pertumbuhan yang membanggakan'
+#output   = stemmer.stem(sentence)
+#
+#print(output)
 # ekonomi indonesia sedang dalam tumbuh yang bangga
 
-print(stemmer.stem('Mereka meniru-nirukannya'))
+print(stemmer.stem('keluhan'))
 # mereka tiru
+
+# ======================= Kelompok Kata & Bobot ============================
+
+nouns = ['jaringan', '4g', '3g', 'koneksi', 'hsdpa', 'h+', 'edge']
+adjs = ['lambat', 'lemot', 'lelet', 'goblok', 'anjing', 'bego', 'cacat']
+customerWords = ['kenapa', 'tolong']
+
+kataOA = ['kait', 'info', 'pantau', 'ketidaknyamanan', 'kendala', 'kena', 'keluh', 'privasi']
+
+# =============================================================================
+
+def filtering (rawDatas):
+# Mencari data mengenai keluhan jaringan dari pelanggan    
+    filteredDatas = []
+    
+    for data in rawDatas:
+        poin = 0
+        output = stemmer.stem(data)
+        
+        words = output.split(' ')
+        for word in words:
+            if word in nouns:
+                poin = poin +1
+                
+            if word in adjs:
+                poin = poin +1
+                
+            if word in customerWords:
+                poin = poin +1
+                
+            if word in kataOA:
+                poin = poin -1
+        
+        if poin > 0:
+            filteredDatas.append(data)
+    
+    return filteredDatas;
+
+
+    
